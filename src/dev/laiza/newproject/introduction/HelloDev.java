5491127732ae4f36f14947fa89ffb9860b0e5941
laiza <laizacristina2507@gmail.com>
2026-05-23 20:12:50 -0300

    My first Java

---
+++ src/dev/laiza/newproject/introduction/HelloDev.java
@@ -0,0 +1,6 @@
+package dev.laiza.newproject.introduction;
+public class HelloDev {
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HAA");
+    }
+}
---
+++ src/dev/laiza/newproject/introduction/TiposPrimitivos.java
@@ -0,0 +1,22 @@
+package dev.laiza.newproject.introduction;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 5000;
+        byte idadeByte = 127;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
+        System.out.println("A idade é "+age+" anos.");
+        System.out.println(falso);
+        System.out.println("char " + caractere);
+        System.out.println(caractere);
+        System.out.println(caractere);
+    }
+}
