279898914b4722d1b1be48adbe06e6b299873ecc
laiza <laizacristina2507@gmail.com>
2026-05-26 16:20:23 -0300

    Estrutura de repetição

---
+++ src/dev/laiza/newproject/introduction/estruturaDeRepeticao4.java
@@ -0,0 +1,16 @@
+package dev.laiza.newproject.introduction;
+
+public class estruturaDeRepeticao4 {
+    // dado o valor do carro, descubra quantas parcelas pode ser pago.
+    // Condição valorParcela> =1000;
+    public static void main(String[] args) {
+       double valorTotal = 30000;
+       for (int parcela = 1; parcela<=valorTotal; parcela++){
+           double valorParcela = valorTotal / parcela;
+           if(valorParcela >= 1000){
+               System.out.println("Parcelas "+ parcela+ " R$"+ valorParcela);
+           }
+       }
+
+    }
+}
